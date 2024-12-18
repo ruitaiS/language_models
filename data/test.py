f37f9c1f1cbd819b5f6ffdc35be953a63430e340
Peter Shao <shaoruitai@gmail.com>
2024-12-17 22:31:22 -0500

    .

--- data/test.py
+++
@@ -1,9 +0,0 @@
-my_list = [1, 2, 3, 4, 5]
-
-for index, value in enumerate(my_list):
-    print(value)
-    my_list[index] = value * 2
-    print(value)
-    #print(f"Index: {index}, Original Value: {value}, Modified Value: {my_list[index]}")
-
-print("Final List:", my_list)
