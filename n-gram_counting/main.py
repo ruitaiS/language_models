53d3c50862ed3ff90552b06420791c3d3d1d4a03
Peter Shao <shaoruitai@gmail.com>
2025-01-18 19:33:51 -0500

    .

--- n-gram_counting/main.py
+++ n-gram_counting/main.py
@@ -1,5 +1,8 @@
 import os
 import nltk
+import math
+import pandas as pd
+
 nltk.data.path.append(os.path.dirname(__file__))
 #nltk.download('punkt_tab', download_dir=os.path.dirname(__file__))
 from nltk.tokenize import word_tokenize
@@ -12,8 +15,8 @@ trigrams = {}
 
 with open('source_text/'+source_text+'/preprocessed.txt', "r") as infile:
     for line in infile:
-        tokens = word_tokenize(line.lower())
-        print("Tokens:", tokens)
+        tokens = word_tokenize(line) # word_tokenize(line.lower())
+        #print("Tokens:", tokens)
         for i in range(len(tokens)):
             if i == 0: # if first
                 unigrams['<s>'] = unigrams.get('<s>', 0) + 1
@@ -34,25 +37,31 @@ with open('source_text/'+source_text+'/preprocessed.txt', "r") as infile:
                     else: #  (more than one more)
                         trigrams[(tokens[i], tokens[i+1], tokens[i+2])] = trigrams.get((tokens[i], tokens[i+1], tokens[i+2]), 0) + 1
 
+unigram_sums = sum(unigrams.values())
+bigram_sums = sum(bigrams.values())
+trigram_sums = sum(trigrams.values())
 
+# Original counts files used log10, so keep that convention here
+unigram_probs = {key: math.log10(count / unigram_sums) for key, count in unigrams.items()}
+bigram_probs = {key: math.log10(count / bigram_sums) for key, count in bigrams.items()}
+trigram_probs = {key: math.log10(count / trigram_sums) for key, count in trigrams.items()}
 
-#print(unigrams)
-#print("Unique Unigrams: " + str(len(unigrams)))
-#print(unigrams['<s>'])
-#print(unigrams['</s>'])
+#print(len(unigram_probs))
+#print(len(bigram_probs))
+#print(len(trigram_probs))
 
-#print(bigrams)
-#print(trigrams)
+unigram_df = pd.DataFrame(list(unigram_probs.items()), columns=['word', 'e']).sort_values(by='e', ascending=False).reset_index(drop=True)
 
+bigram_df = pd.DataFrame(
+    [(key[0], key[1], value) for key, value in bigram_probs.items()],
+    columns=["n", "n+1", "e"]
+).sort_values(by='e', ascending=False).reset_index(drop=True)
 
-# TODO: remember data.py does this transformation
-'''
-unigram = pd.read_csv('unigram_counts.txt', sep=' ', header=None).rename(columns={0: 'x_0', 1: 'e'})
-bigram = pd.read_csv('bigram_counts.txt', sep=' ', header=None).rename(columns={0: 'x_i', 1: 'x_j', 2:'e'})
-trigram = pd.read_csv('trigram_counts.txt', sep=' ', header=None).rename(columns={0: 'x_i', 1: 'x_j', 2: 'x_k', 3:'e'})
+trigram_df = pd.DataFrame(
+    [(key[0], key[1], key[2], value) for key, value in trigram_probs.items()],
+    columns=["n", "n+1", "n+2", "e"]
+).sort_values(by='e', ascending=False).reset_index(drop=True)
 
-unigram['P'] = unigram['e'].apply(lambda e: 10**e)
-bigram['P'] = bigram['e'].apply(lambda e: 10**e)
-trigram['P'] = trigram['e'].apply(lambda e: 10**e)
-
-'''
+print(unigram_df)
+print(bigram_df)
+print(trigram_df)
