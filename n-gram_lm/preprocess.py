3f962849536aedf2ce8e520a1a27a1eb1847319e
Peter Shao <shaoruitai@gmail.com>
2025-01-25 16:47:42 -0500

    fixed lp calculations

--- n-gram_lm/preprocess.py
+++ n-gram_lm/preprocess.py
@@ -15,9 +15,9 @@ from nltk.tokenize import word_tokenize
 # Might be interesting to explore sentence sequences. Maybe some groups of sentences go together
 
 input_file = "text/akjv.txt"
-unigrams = {}
-bigrams = {}
-trigrams = {}
+unigram_counts = {}
+bigram_counts = {}
+trigram_counts = {}
 
 with open(input_file, "r") as infile:
 
@@ -50,51 +50,52 @@ for line in train_set:
         tokens = word_tokenize(text) #word_tokenize(text.lower())
         #print("Tokens:", tokens)
         for i in range(len(tokens)):
-            unigrams[tokens[i]] = unigrams.get(tokens[i], 0) + 1
+            unigram_counts[tokens[i]] = unigram_counts.get(tokens[i], 0) + 1
             if i == 0: # First token is the second character of a bigram starting with '<s>'
-                unigrams['<s>'] = unigrams.get('<s>', 0) + 1
-                bigrams[('<s>',tokens[i])] = bigrams.get(('<s>',tokens[i]), 0) + 1            
+                unigram_counts['<s>'] = unigram_counts.get('<s>', 0) + 1
+                bigram_counts[('<s>',tokens[i])] = bigram_counts.get(('<s>',tokens[i]), 0) + 1            
                 if i < len(tokens) - 1: # (at least one more)
-                    trigrams[('<s>',tokens[i], tokens[i+1])] = trigrams.get(('<s>',tokens[i], tokens[i+1]), 0) + 1
+                    trigram_counts[('<s>',tokens[i], tokens[i+1])] = trigram_counts.get(('<s>',tokens[i], tokens[i+1]), 0) + 1
                 else: # (first and also last)
-                    trigrams[('<s>',tokens[i], '</s>')] = trigrams.get(('<s>',tokens[i], '</s>'), 0) + 1
+                    trigram_counts[('<s>',tokens[i], '</s>')] = trigram_counts.get(('<s>',tokens[i], '</s>'), 0) + 1
             else:
                 if i == len(tokens) - 1: # if no more
-                    bigrams[(tokens[i], '</s>')] = bigrams.get((tokens[i], '</s>'), 0) + 1
-                    unigrams['</s>'] = unigrams.get('</s>', 0) + 1
+                    bigram_counts[(tokens[i], '</s>')] = bigram_counts.get((tokens[i], '</s>'), 0) + 1
+                    unigram_counts['</s>'] = unigram_counts.get('</s>', 0) + 1
                 else: # at least one more
-                    bigrams[(tokens[i], tokens[i+1])] = bigrams.get((tokens[i], tokens[i+1]), 0) + 1
+                    bigram_counts[(tokens[i], tokens[i+1])] = bigram_counts.get((tokens[i], tokens[i+1]), 0) + 1
                     if i == len(tokens) - 2: # (exactly one more)
-                        trigrams[(tokens[i], tokens[i+1], '</s>')] = trigrams.get((tokens[i], tokens[i+1], '</s>'), 0) + 1
+                        trigram_counts[(tokens[i], tokens[i+1], '</s>')] = trigram_counts.get((tokens[i], tokens[i+1], '</s>'), 0) + 1
                     else: #  (more than one more)
-                        trigrams[(tokens[i], tokens[i+1], tokens[i+2])] = trigrams.get((tokens[i], tokens[i+1], tokens[i+2]), 0) + 1
+                        trigram_counts[(tokens[i], tokens[i+1], tokens[i+2])] = trigram_counts.get((tokens[i], tokens[i+1], tokens[i+2]), 0) + 1
     else:
         print(f'Unprocessed Line: {line}')
 
-# Create Probabilities hash map
-unigram_sums = sum(unigrams.values())
-bigram_sums = sum(bigrams.values())
-trigram_sums = sum(trigrams.values())
+print(f"{len(unigram_counts)} unigrams in training set")
+print(f"{len(bigram_counts)} bigrams in training set")
+print(f"{len(trigram_counts)} trigrams in training set")
 
-print(f"{len(unigrams)} unigrams in training set")
-print(f"{len(bigrams)} bigrams in training set")
-print(f"{len(trigrams)} trigrams in training set")
+# Create Probabilities hash map
+unigram_sum = sum(unigram_counts.values())
+unigram_probs = {unigram: count / unigram_sum for unigram, count in unigram_counts.items()}
+bigram_probs = {bigram: count / unigram_counts[bigram[0]] for bigram, count in bigram_counts.items()}
+trigram_probs = {trigram: count / bigram_counts[(trigram[0], trigram[1])] for trigram, count in trigram_counts.items()}
 
 # Normalize Probabilities
 # Previous n-gram files (and the existing code to process them) use log10(P), so keep that convention here
 # It's to make multiplication easier bc you can just add them as logs then exponentiate at the end
-unigrams = {x: math.log10(count / unigram_sums) for x, count in unigrams.items()}
-bigrams = {x: math.log10(count / bigram_sums) for x, count in bigrams.items()}
-trigrams = {x: math.log10(count / trigram_sums) for x, count in trigrams.items()}
+unigram_lp = {unigram: math.log10(prob) for unigram, prob in unigram_probs.items()}
+bigram_lp = {bigram: math.log10(prob) for bigram, prob in bigram_probs.items()}
+trigram_lp = {trigram: math.log10(prob) for trigram, prob in trigram_probs.items()}
 
 # Assign index mapping and create vocab hash
-xft = {token: index + 1 for index, token in enumerate(sorted(unigrams.keys()))} # xft eg. index from token
+xft = {token: index + 1 for index, token in enumerate(sorted(unigram_lp.keys()))} # xft eg. index from token
 vocab = {b:a for a,b in xft.items()} # tfx token from index
 
 # Convert counts hashes to all use indexes instead of token strings
-unigrams = {xft[unigram] : logprob for unigram, logprob in unigrams.items()}
-bigrams = {(xft[bigram[0]], xft[bigram[1]]): logprob for bigram, logprob in bigrams.items()}
-trigrams = {(xft[trigram[0]], xft[trigram[1]], xft[trigram[2]]): logprob for trigram, logprob in trigrams.items()}
+unigram_lp = {xft[unigram] : logprob for unigram, logprob in unigram_lp.items()}
+bigram_lp = {(xft[bigram[0]], xft[bigram[1]]): logprob for bigram, logprob in bigram_lp.items()}
+trigram_lp = {(xft[trigram[0]], xft[trigram[1]], xft[trigram[2]]): logprob for trigram, logprob in trigram_lp.items()}
 
 # TODO sort so humans can read too
 
--- n-gram_lm/tune.py
+++ n-gram_lm/tune.py
@@ -16,13 +16,15 @@ def get_index(t_i):
     return xft[t_i]
   else:
     # TODO: Decide on how to handle upper / lowercase versions
+    # I think it should fall back to lowercase if it can't find the uppercase
+    # (?) store probabilities for both cases, and compute combined when needed
     #print(f"Not in vocab: {t_i}")
     return -1
 
 # Loss Function:
 def negative_log_likelihood(lambdas, epsi = 1e-10):
   l1, l2, l3 = lambdas
-  print(f"l1: {l1}, l2: {l2}, l3: {l3}")
+  print(f"l1: {lambdas[0]}, l2: {lambdas[1]}, l3: {lambdas[2]}")
   total_likelihood = 0
 
   # TODO: I think epsi gets doubled (if there's no bigram, there's obviously no trigram)
@@ -32,42 +34,42 @@ def negative_log_likelihood(lambdas, epsi = 1e-10):
       x_i = get_index(t_i)
 
       # TODO: double check log prob and raw prob
-      fallback = -100 # equiv to 1e-100 raw prob
+      fallback = -1000 # equiv to 1e-100 raw prob
       if x_i == -1:
         print(f"Word {t_i} not in vocab")
         unigram_prob = fallback # log prob
-        bigram_prob,l2 = 0,0 # dropout value, not a probability
-        trigram_prob,l3 = 0,0 # drop
+        bigram_prob = fallback #
+        trigram_prob = fallback #
       else:
         unigram_prob = unigram[x_i] # log prob
         if i == 0: # First word in the sentence
           # TODO: Does this mass amount of guaranteed correct first guesses introduce bias towards unigrams in tuning?
-          print(f"i: {i}, t_i: {t_i}, unigram_prob: {10**unigram_prob}") # raw prob for print
-          bigram_prob,l2 = 0,0 # drop
-          trigram_prob,l3 = 0,0 # drop
+          # print(f"i: {i}, t_i: {t_i}, unigram_prob: {10**unigram_prob}") # raw prob for print
+          bigram_prob = fallback
+          trigram_prob = fallback
         else:
           x_i_min_1 = get_index(sentence[i-1])
-          bigram_prob = bigram.get((x_i_min_1, x_i), -100) # log prob
+          bigram_prob = bigram.get((x_i_min_1, x_i), -1000) # log prob
 
           # Check it's defaulting properly
           if x_i_min_1 == -1:
             print(f"Could not find bigram starting with {sentence[i-1]}. bigram_prob: {10**bigram_prob}") # raw prob
-            bigram_prob,l2 = 0,0 # drop
-          else:
-            bigram_prob,l2 = bigram_prob,lambdas[1]
+            #bigram_prob = fallback
+          #else:
+            #bigram_prob,l2 = bigram_prob,lambdas[1]
 
           if i > 1:
             x_i_min_2 = sentence[i-2]
-            trigram_prob = trigram.get((x_i_min_2, x_i_min_1, x_i), -100)
+            trigram_prob = trigram.get((x_i_min_2, x_i_min_1, x_i), -1000)
             if x_i_min_2 == -1:
               print(f"Could not find trigram starting with {sentence[i-2]}. trigram_prob: {10**trigram_prob}") # raw prob
-              trigram_prob,l3 = 0,0 # drop
-            else:
-              trigram_prob,l3 = trigram_prob,lambdas[2]
           
       # Interpolate + update
-      prob = math.log10(l1 * (10**unigram_prob) + l2 * (10**bigram_prob) + l3 * (10**trigram_prob))
-      total_likelihood -= prob
+      print(f"Uni: {unigram_prob}, Bi: {bigram_prob}, Tri: {trigram_prob}")
+      raw_prob = l1 * (10**unigram_prob) + l2 * (10**bigram_prob) + l3 * (10**trigram_prob)
+      print(max(raw_prob, fallback))
+      log_prob = math.log10(max(raw_prob, fallback))
+      total_likelihood += log_prob
   
   print(total_likelihood/len(observed_set))
   return (total_likelihood/len(observed_set))
