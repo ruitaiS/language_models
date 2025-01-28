5f65120d305e888456f3921aa6c721bec2ab4bc7
Peter Shao <shaoruitai@gmail.com>
2025-01-27 22:13:33 -0500

    unigram dropout for punctuation

--- n-gram_lm/generate.py
+++ n-gram_lm/generate.py
@@ -1,36 +1,34 @@
 import data
 import random
+import math
 
 unigram = data.get_unigram()
 bigram = data.get_bigram()
 trigram = data.get_trigram()
-xft, tfx = data.get_lookups() # index from token, token from index
+xft, tfx = data.get_lookups()
 
-#l1, l2, l3 = (9.49650121e-01, 9.37157253e-18, 5.03498791e-02)
-
-#l1 = 0.19294876040543507
-#l2 = 0.8070512544957388
-#l3 = 0.0
-
-#l1, l2, l3=(1.57446217e-01, 8.42553783e-01, 1.36187719e-14)
-
-l1, l2, l3 = (0.14434065, 0.48567127, 0.36998808)
+#l1, l2, l3 = (0.14051555, 0.48004991, 0.37943454)
+l1, l2, l3 = (0.11508279, 0.50076896, 0.38414825)
 fallback = -100
 
 def output(max_tokens=200):
 	prob_distribution = {}
 	sentence = [xft['<s>']]
 	while len(sentence) < max_tokens:
-		for index in tfx.keys():
-			unigram_lp = unigram.get(index, fallback)
+		for index in tfx.keys(): # Iterate over all possible next words
 			if len(sentence) == 1:
 				bigram_lp = bigram.get((xft['<s>'], index), fallback)
-				prob = l1 * (10**unigram_lp) + l2 * (10**bigram_lp)
+				prob = l2 * (10**bigram_lp)
 			else:
+				unigram_lp = unigram.get(index, fallback)
 				bigram_lp = bigram.get((sentence[-1], index), fallback)
 				trigram_lp = trigram.get((sentence[-2], sentence[-1], index), fallback)
-				prob = l1 * (10**unigram_lp) + l2 * (10**bigram_lp) + l3 * (10**trigram_lp)
-			prob_distribution[index] = prob
+				print(f"Type: {type(index)}, value: {index}")
+				if int(index) <= 10: # Ignore unigram probs for punctuation
+					prob = l2 * (10**bigram_lp) + l3 * (10**trigram_lp)
+				else:
+					prob = l1 * (10**unigram_lp) + l2 * (10**bigram_lp) + l3 * (10**trigram_lp)
+			prob_distribution[index] = prob ## TODO: Verify super janky softmax
 		#print(sum(prob_distribution.values()))
 
 		# TODO: This sampling method is psychotic and mathematically unsound
@@ -47,9 +45,6 @@ def output(max_tokens=200):
 			break
 	return sentence
 			
-
-
-
 index_list = output()
 readable_list = [tfx[index] for index in index_list]
 print(" ".join(readable_list))
--- n-gram_lm/tune.py
+++ n-gram_lm/tune.py
@@ -1,93 +1,64 @@
-import pandas as pd
-import numpy as np
 import data
 import math
 from scipy.optimize import minimize
 
-#vocab = data.get_vocab()
-unigram = data.get_unigram() # (index) : log prob
-bigram = data.get_bigram() # (index, index) : log prob
-trigram = data.get_trigram() # (index, index, index) : log prob
-xft, tfx = data.get_lookups() # index from token, token from index
+unigram = data.get_unigram()
+bigram = data.get_bigram()
+trigram = data.get_trigram()
+xft, tfx = data.get_lookups()
 observed_set = data.get_dev_set()
 
 def get_index(t_i):
   if t_i in xft:
     return xft[t_i]
   else:
-    # TODO: Decide on how to handle upper / lowercase versions
-    # I think it should fall back to lowercase if it can't find the uppercase
-    # (?) store probabilities for both cases, and compute combined when needed
-    #print(f"Not in vocab: {t_i}")
     return -1
 
 # Loss Function:
-def negative_log_likelihood(params): # equiv to 1e-1000 raw prob
+def negative_log_likelihood(params):
   l1, l2, l3, fallback = params
   ##print(f"l1: {params[0]}, l2: {params[1]}, l3: {params[2]}, fallback: {params[3]}")
-  total_likelihood = 0
-
+  set_likelihood = 0
   not_in_vocab = 0
 
   for sentence in observed_set:
     sentence_likelihood = 0
-    # t_i for token string representation, x_i for vocab index representation
     for i, t_i in enumerate(sentence):
       x_i = get_index(t_i)
       if x_i == -1:
         #print(f"Word {t_i} not in vocab")
         not_in_vocab += 1
-        unigram_prob = fallback # log prob
-        bigram_prob = fallback #
-        trigram_prob = fallback #
+        raw_prob = fallback
       else:
-        unigram_prob = unigram.get(x_i, fallback) # log prob
-        # Note: <s> and </s> are removed from unigram prob
-        # fallback value is needed even though these tokens exist in vocab
-        #print(f"unigram: {unigram.get(x_i, fallback)}, fallback: {fallback}")
         if i == 1: # First word in the sentence after <s>
           bigram_prob = bigram.get((xft['<s>'], x_i), fallback)
-          trigram_prob = fallback
+          raw_prob = l2*(10**bigram_prob)
         else:
           x_i_min_1 = get_index(sentence[i-1])
-          bigram_prob = bigram.get((x_i_min_1, x_i), fallback) # log prob
           x_i_min_2 = get_index(sentence[i-2])
+          unigram_prob = unigram.get(x_i, fallback)
+          bigram_prob = bigram.get((x_i_min_1, x_i), fallback)
           trigram_prob = trigram.get((x_i_min_2, x_i_min_1, x_i), fallback)
-
-          if bigram_prob > fallback:
-            print(f"Nonzero Bigram: {bigram_prob}")
-
-          if trigram_prob > fallback:
-            print(f"Nonzero Trigram: {trigram_prob}")
-      #print(f"Uni: {unigram_prob}, Bi: {bigram_prob}, Tri: {trigram_prob}")
-      raw_prob = l1 * (10**unigram_prob) + l2 * (10**bigram_prob) + l3 * (10**trigram_prob)
-      #print(f"l1: {l1}, l2: {l2}, l3: {l3}")
-      #print(f"Raw: {raw_prob}, fallback: {1e-10}, max: {max(raw_prob, 1e-10)}")
+          if int(x_i) <= 10: # Ignore unigram probs for punctuation
+            raw_prob = l2 * (10**bigram_prob) + l3 * (10**trigram_prob)
+          else:
+            raw_prob = l1 * (10**unigram_prob) + l2 * (10**bigram_prob) + l3 * (10**trigram_prob)
       log_prob = math.log10(max(raw_prob, 1e-10))
       sentence_likelihood += log_prob
-    total_likelihood += sentence_likelihood
+    set_likelihood += sentence_likelihood
   ##print(f"P: {total_likelihood/len(observed_set)}, Not In Vocab: {not_in_vocab}")
-  return -(total_likelihood/len(observed_set))
+  return -(set_likelihood/len(observed_set))
 
 
-# Optimization
+# Optimization - Minimize NLL
 initial_params = [1/3, 1/3, 1/3, -100]
 constraints = [{'type': 'eq', 'fun': lambda x: sum(x[:3]) - 1}]  # l1 + l2 + l3 = 1
-bounds = [(0, 1), (0, 1), (0, 1), (-1000, 0)]  # Each lambda must be between 0 and 1, fallback can go to -1000
-
-# Minimize the negative log-likelihood
+bounds = [(0, 1), (0, 1), (0, 1), (-1000, 0)]
 result = minimize(negative_log_likelihood, initial_params, 
                   constraints=constraints, bounds=bounds)
 
 print(f"Success? : {result.success}")
 print(f"Message : {result.message}")
 
-# Optimal weights
 optimal_lambdas = result.x
-print("Optimal lambdas:", optimal_lambdas)
-
-# [9.49650121e-01 9.37157253e-18 5.03498791e-02]
-#l1: 0.19294876040543507, l2: 0.8070512544957388, l3: 0.0
-
-#testval = negative_log_likelihood(initial_lambdas)
-#print(testval)
+print(f"Optimal lambdas: {optimal_lambdas[:3]}, optimal fallback: {optimal_lambdas[3]}")
