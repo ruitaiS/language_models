00833d7e1aea0496cfd57ead5d30ba4372e02c66
Peter Shao <shaoruitai@gmail.com>
2025-09-16 17:52:45 -0400

    .

--- rnn/generate.py
+++ rnn/generate.py
@@ -1,8 +1,34 @@
 import os
+import sys
 from rnn import load_rnn_model, sample
 
-filepath = os.path.join('models',  'v6', 'epoch_50.net')
-model, optimizer = load_rnn_model(filepath)
+if len(sys.argv) > 1:
+    model_version = sys.argv[1]
+else:
+    model_version = 'v6'
+
+files = [f for f in os.listdir(os.path.join('models', model_version))
+         if f.startswith("epoch_") and f.endswith(".net")]
+if not files:
+    raise FileNotFoundError(f"No model files found in {model_version}")
+
+latest_epoch = int(max(files, key=lambda f: int(f[6:-4]))[6:-4])
+model_filename = f"epoch_{latest_epoch}.net"
+if len(sys.argv) > 2:
+    model_filename = f"epoch_{int(sys.argv[2])}.net"
+
+#---------------------------------------------------------------------------
+
+filepath = os.path.join('models',  model_version, model_filename)
+try:
+    model, optimizer = load_rnn_model(filepath)
+except FileNotFoundError as e:
+        print(f"models/{model_version}/{model_filename} not found.")
+        print(f"Available in {model_version}:")
+        print(files)
+        sys.exit(1)
+
+print(f"Loading model {model_version} epoch {int(model_filename[6:-4])}")
 print(f"\nModel: {model}")
 
 text = sample(model, stop_char='\n', prime='\t', temperature=1.0)
---
+++ rnn/plot.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import re
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if len(sys.argv) == 2:
+    user_input = sys.argv[1]
+else:
+    user_input = 'v6'
+path = os.path.join('models', user_input)
+
+df_rows = []
+for filename in os.listdir(path):
+    if filename.startswith("epoch_") and filename.endswith(".meta"):
+        match = re.match(r"epoch_(\d+)\.meta", filename)
+        if not match:
+            continue
+        epoch = int(match.group(1))
+
+        with open(os.path.join(path, filename), "r") as f:
+            data = json.load(f)
+
+        epoch_losses = data['epoch_losses']
+        val_loss = data.get('val_loss', 0)
+
+        for loss in epoch_losses:
+            df_rows.append({
+                "epoch": epoch,
+                "loss": loss,
+                "val_loss": val_loss,
+                })
+
+if len(df_rows) == 0:
+    print(f"No metadata found in models/{user_input}")
+else:
+
+    df = pd.DataFrame(df_rows)
+    df = df.sort_values(['epoch'], kind="stable").reset_index(drop=True)
+    df['total_batches'] = range(1, len(df)+1)
+
+    #-----------------
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(df['total_batches'], df['loss'], label="Training Loss")
+    plt.plot(df['total_batches'], df['val_loss'], label="Validation Loss")
+
+    epoch_bounds = df.groupby("epoch")["total_batches"].min()
+    for epoch, bound in epoch_bounds.items():
+        plt.axvline(x=bound, color='gray', linestyle='--', alpha=0.5)
+    plt.xticks(epoch_bounds, labels=epoch_bounds.index)
+
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title(f"RNN Model {user_input}")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
