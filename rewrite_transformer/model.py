4674e0b450021a86960a866c9884de60c8df4fe7
Ruitai Shao <shaoruitai@gmail.com>
2025-11-23 09:08:25 -0500

    cleanup, simplify, add dropout

--- rewrite_transformer/model.py
+++ rewrite_transformer/model.py
@@ -11,125 +11,81 @@ class EmbeddingLayer(nn.Module):
         self.register_buffer("positions", torch.arange(context_len).unsqueeze(0))
 
     def forward(self, token_batch, seq_len):
-        # Accepts one batch of tokens, shape (batch_size, seq_len), where seq_len <= context_len
-        # tokens = torch.tensor([1, 2, 3, 4], [5, 6, 7, 8]) >> tokens.shape = (2, 4)
-        X = self.E(token_batch) + self.P(self.positions[:, :seq_len]) # Composite Embeddings (Word + position)
-        #print(f"embedding_layer(tokens): {X}")
-
-        # X.shape = (batch_size, seq_len,embedding_dim)
-        return X
-
-class LayerNorm(nn.Module):
-    # TODO: You might want to just use nn.LayerNorm
-    def __init__(self, d):
-        super().__init__()
-        self.gain = nn.Parameter(torch.ones(d))
-        self.offset = nn.Parameter(torch.zeros(d))
-        #print('layernorm init')
-    def forward(self, X):
-        # X.shape = (batch_size, seq_length, embedding dimension d)
-        # Normalize each d-length embedding vector in X:
-        means = X.mean(dim=-1, keepdim=True) # dim=-1 is d dimension
-        sdevs = X.std(dim=-1, unbiased=False, keepdim=True)
-        X = self.gain * ((X - means) / (sdevs + 1e-9)) + self.offset
-        #print(f'layernorm(X): {X}')
-        return X
+        return self.E(token_batch) + self.P(self.positions[:, :seq_len])
     
 class FFN(nn.Module): # Feed Forward Network
-    # TODO: You might want to simplify this to a nn.Sequential(linear, relu, linear)
-    # you're rolling your own linear layers here with manual weights and biases lol
-
-    def __init__(self, d, d_hidden = None):
+    def __init__(self, d, expansion_ratio=4):
         super().__init__()
-        # d_hidden is the number of nodes / dimensions on the hidden layer
-        # (Book calls this dff)
-        # W1, b1 take us to hidden layer
-        # W2, b2 take us back to number of embed feature dimensions d
-        # d >> dff >> d
-        if not d_hidden or d_hidden <= d:
-            d_hidden = 4*d
+        assert expansion_ratio>1, "FFN expansion ratio must produce a larger hidden dimension"
 
-        self.W1 = nn.Parameter(torch.randn(d, d_hidden))
-        self.b1 = nn.Parameter(torch.zeros(d_hidden))
-
-        self.W2 = nn.Parameter(torch.randn(d_hidden, d))
-        self.b2 = nn.Parameter(torch.zeros(d))
-        #print('ffn init')
+        self.network = nn.Sequential(
+            nn.Linear(d, int(d*expansion_ratio)),
+            nn.ReLU(), # TODO: toggle for nn.GeLU() # nn.SiLU()
+            nn.Linear(int(d*expansion_ratio), d),
+        )
 
     def forward(self, X):
-        # ReLu(xW1+b1)W2 + b2
-        hidden = torch.relu(torch.matmul(X, self.W1) + self.b1) # ReLu(xW1 + b1)
-        output = torch.matmul(hidden, self.W2) + self.b2 # hidden * W2 + b2
-        #print(f'ffn(X): {output}')
-        return output
+        return self.network(X)
 
 class TransformerBlock (nn.Module):
-    # TODO: dropout
-    def __init__(self, d, total_heads):
+    # TODO: more intuitive dropout parameter passing
+    def __init__(self, d, total_heads, ffn_expansion_ratio=4, attention_head_dropout=0.5, post_mha_dropout=0.5, post_ffn_dropout=0.5):
         super().__init__()
 
-        self.norm1 = LayerNorm(d)
-        self.norm2 = LayerNorm(d)
-        self.ffn = FFN(d)
-        
-        # TODO: Make sure it works with these library ones first
-        '''
-        self.norm1 = nn.LayerNorm(d)
-        self.norm2 = nn.LayerNorm(d)
-        self.ffn = nn.Sequential(
-            nn.Linear,
-            nn.ReLU,
-            nn.Linear)
-        '''
+        self.norm1 = nn.LayerNorm(d, eps=1e-5, elementwise_affine=True)
+        self.norm2 = nn.LayerNorm(d, eps=1e-5, elementwise_affine=True)
+        self.dropout1 = nn.Dropout(p=post_mha_dropout)
+        self.dropout2 = nn.Dropout(p=post_ffn_dropout)
+        self.mha = MHA(d, total_heads, attention_head_dropout)
+        self.ffn = FFN(d, ffn_expansion_ratio)
 
-        self.mha = MHA(d, total_heads)
-        #print('transformer init')
     def forward(self, X, attention_mask):
-        # pg. 10
-        # with input x:
+        # Note pre-norm architecture; slightly modernized from original 2017 paper
+        # TODO: worth writing a little on why it's better this way vs. the original spec
+        # mainly allows direct gradient flow through residuals
+
         residual = X
         X = self.norm1(X)
         X = self.mha(X, attention_mask)
-        X += residual
+        X = self.dropout1(X) + residual
+
         residual = X
         X = self.norm2(X)
         X = self.ffn(X)
-        X += residual
-        #print(f"transformer(X) : {X}")
-        #print(f"Transformer Block Output Shape: {X.shape}")
+        X = self.dropout2(X) + residual
         return X
 
 class MHA(nn.Module): # Multi-Headed Attention
-    def __init__(self, d, total_heads):
+    def __init__(self, d, total_heads, attention_head_dropout=0.5):
         super().__init__()
         assert d % total_heads == 0, "d must be divisible by total number of heads"
         self.d_h = d // total_heads # instantiate SHAs with d_k and d_v set to d_h
-        self.heads = nn.ModuleList([SHA(d, self.d_h, self.d_h) for _ in range(total_heads)])
+        self.heads = nn.ModuleList([SHA(d, self.d_h, self.d_h, attention_head_dropout) for _ in range(total_heads)])
         self.W_0 = nn.Linear(d, d)
-        #print('mha init')
     def forward(self, X, attention_mask):
         output = torch.cat([head(X, attention_mask) for head in self.heads], dim=-1)
         output = self.W_0(output)
-        #print(f'mha(X): {output}')
         return output
 
 class SHA(nn.Module): # Single Head Attention
     # d_k and d_v also known as head dimension, d_h, in MHA context
-    def __init__(self, d, d_k, d_v):
+    def __init__(self, d, d_k, d_v, attention_head_dropout=0.5):
         super().__init__()
         self.W_Q = nn.Linear(d, d_k)
         self.W_K = nn.Linear(d, d_k)
         self.W_V = nn.Linear(d, d_v)
-        self.scaling = math.sqrt(d_k)
+        self.dropout = nn.Dropout(p=attention_head_dropout)
+        self.scaling = 1/math.sqrt(d_k)
 
     def forward(self, X, attention_mask):
         Q = self.W_Q(X) # Queries Matrix (batch_size, seq_len, d_k)
         K = self.W_K(X) # Keys Matrix (batch_size, seq_len, d_k)
         V = self.W_V(X) # Values Matrix (batch_size, seq_len, d_v)
 
-        scores = (Q @ K.transpose(-2, -1)) / self.scaling
+        scores = (Q @ K.transpose(-2, -1)) * self.scaling
         scores = scores.masked_fill(~attention_mask, float('-1e9'))
         weights = torch.softmax(scores, dim=-1)
+        weights = self.dropout(weights)
         product = torch.matmul(weights, V)
         return product
 
@@ -137,8 +93,6 @@ class LanguageModel(nn.Module):
     def __init__(self, context_len, embedding_dim, num_layers, total_heads, vocab_size, pad_token_idx):
         super().__init__()
 
-        # TODO: These need to be stored when saving the model so it can be re-instantiated
-        # context_len is the only one that really needs to be read off the model (used by generation function to truncate the input)
         self.vocab_size = vocab_size
         self.context_len = context_len
         self.pad_token_idx = pad_token_idx
@@ -147,8 +101,19 @@ class LanguageModel(nn.Module):
         self.total_heads = total_heads
         self.register_buffer("causal_mask", torch.tril(torch.ones(context_len, context_len, dtype=torch.bool)))
 
+        # TODO as params
+        embedding_dropout = 0.5
+        ffn_expansion_ratio=4
+        post_mha_dropout=0.5
+        post_ffn_dropout=0.5
+        attention_head_dropout=0.5
+
         self.embedding_layer = EmbeddingLayer(embedding_dim, vocab_size, context_len)
-        self.transformer_layers = nn.ModuleList([TransformerBlock(embedding_dim, total_heads) for _ in range(num_layers)])
+        self.dropout = nn.Dropout(p=embedding_dropout)
+        self.transformer_layers = nn.ModuleList(
+            [TransformerBlock(embedding_dim, total_heads, ffn_expansion_ratio,
+                              attention_head_dropout, post_mha_dropout, post_ffn_dropout)
+                              for _ in range(num_layers)])
 
     def forward(self, token_batch):
         # Create attention mask
@@ -163,7 +128,8 @@ class LanguageModel(nn.Module):
 
         # Pass Through Model:
         X = self.embedding_layer(token_batch, seq_len)
-        #for layer in self.transformer_layers:
-        #    X = layer(X, attention_mask)
+        X = self.dropout(X)
+        for layer in self.transformer_layers:
+            X = layer(X, attention_mask)
         logits = torch.matmul(X, self.embedding_layer.E.weight.T)
         return logits
--- rewrite_transformer/train.py
+++ rewrite_transformer/train.py
@@ -26,11 +26,11 @@ def calculate_loss(loss_function, logits, targets):
     flat_logits = logits.view(-1, logits.shape[-1])
     flat_targets = targets.view(-1)
 
-    print("logits stats: min", logits.min().item(),
-      "max", logits.max().item(),
-      "mean", logits.mean().item(),
-      "std", logits.std().item())
-    print("targets range:", flat_targets.min().item(), "to", flat_targets.max().item())
+    #print("logits stats: min", logits.min().item(),
+    #  "max", logits.max().item(),
+    #  "mean", logits.mean().item(),
+    #  "std", logits.std().item())
+    #print("targets range:", flat_targets.min().item(), "to", flat_targets.max().item())
 
     return loss_function(flat_logits, flat_targets)
 
@@ -141,7 +141,7 @@ for i in range(epochs):
         if (j < 5 or j % print_interval == 0 or j == training_batches-1):
             # TODO: Avg. Loss Over Validation Set
             print(f"\n Epoch {i+1} / {epochs} || {j} / {training_batches-1} || {(time.time() - start):.3f}s || Loss: {loss :.3f}")
-            #generate.test_generate(model, tokenizer)
+            generate.test_generate(model, tokenizer)
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         clip_grad_norm_(model.parameters(), max_norm=1.0)
